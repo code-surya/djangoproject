b44328c131f4556b5b334551e353b2072052d394
Code_surya <suryapratapsinghg@gmail.com>
2022-03-22 14:41:02 +0530

    Registering Post model with django admin

--- feed/models.py
+++ feed/models.py
@@ -3,3 +3,16 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=240)
+    def __str__(self) :
+        return self.text[0:100]
+
+
+
+
+
+
+
+
+
+
+
