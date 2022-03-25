ad262a370995eac23c4b49e8e3f5f9fdcde2ad1e
Code_surya <suryapratapsinghg@gmail.com>
2022-03-25 14:15:04 +0530

    Creating new Post

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -21,3 +22,19 @@ class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "feed/create.html"
     fields = ['text']
+    success_url = "/"
+
+
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+
+
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save
+        return super().form_valid(form)
